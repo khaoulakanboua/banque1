6014e2138390d5322ab679dfa77c3a0e80a10287
AnasJeg <anasjegoual.2000@gmail.com>
2023-05-08 23:36:47 +0100

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+import { Menubar } from 'primereact/menubar';
+import logo from "../assets/bank-logo.png"
+
+export default function Header() {
+    const navigate = useNavigate();
+
+    const items = [
+        {
+            label: 'Home',
+            command: () => {navigate('/') }
+        },
+        {
+            label: 'Agence',
+            command: () => {navigate('/agence') }
+        },
+        
+
+
+    ];
+    const style = {
+        backgroundColor: 'rgba(245,243,246,0.88)',
+        color: '#230202',
+        borderRadius:"20px",
+        justifyContent: 'left'
+    };
+
+
+    const end = (
+        <img
+            alt="logo"
+            src={logo}
+            width="50px"
+            className="mr-2"
+        />
+    );
+
+
+
+    return (
+        <div>
+            <div className="card">
+                <Menubar  end={end} style={style}  model={items}  />
+
+            </div>
+        </div>
+    );
+
+}
