85d346259d7060e0f282597f4c6bb8da157c8354
AnasJeg <anasjegoual.2000@gmail.com>
2023-05-09 01:55:27 +0100

    afterkhpull

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AgenceList from './components/agenceList';
-import AgenceForm from './components/agenceForm';
+
 function App() {
   return (
     <Router>
     <div className="App">
-   
-
-       <Routes>
-          <Route path="/agence" element={<AgenceList/>} />
-          <Route path="/addAgence" element={<AgenceForm/>} />
-
-
-
-
-
-
-        </Routes>
-       
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
 
 
