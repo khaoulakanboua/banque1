35a36c176e4193e75fbb224bcfff5966a6f8dd01
AnasJeg <anasjegoual.2000@gmail.com>
2023-05-29 23:42:44 +0100

    testF

--- src/auth/ProRoutes.jsx
+++ src/auth/ProRoutes.jsx
@@ -11,6 +11,7 @@ import Compte from '../pages/Compte';
 import Banque from '../pages/Banque';
 import Operation from '../pages/Operation';
 import Banque1 from '../pages/Banque1';
+import ErrorPage from '../pages/ErrorPage';
 
 export default function ProRoutes() {
   return (
@@ -50,6 +51,7 @@ export default function ProRoutes() {
                 <Route path='Banque1' element={<Banque1 />} />
                  )}
             </Route>
+            <Route path='*' element={<ErrorPage/>} />
         </Routes>
         </div>
 
